8292ae65a2b8675efcc497f31e73ff4f1ff5a5be
李文科(10022680) <liwk-b@glodon.com>
2019-11-19 11:05:50 +0800

    first commit

---
+++ src/main/java/com/glodon/MybatisGeneratoApplication.java
@@ -0,0 +1,13 @@
+package com.glodon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisGeneratoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glodon/MybatisGeneratoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glodon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
